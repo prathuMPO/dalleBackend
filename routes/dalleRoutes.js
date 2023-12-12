ab2d044a3af55f0c1b60f6c31aa1615faef111f9
Purvish V Mangroliya <prathammangroliya60@gmail.com>
2023-12-11 21:06:44 -0500

    server done

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -20,6 +20,8 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
+    console.log('Received prompt:', prompt);
+
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
@@ -27,11 +29,13 @@ router.route('/').post(async (req, res) => {
       response_format: 'b64_json',
     });
 
+    console.log('AI Response:', aiResponse.data);
+
     const image = aiResponse.data.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    console.error('Error:', error);
+    res.status(500).send(error?.response?.data?.error?.message || 'Something went wrong');
   }
 });
 
